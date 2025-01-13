1d62da0cc8ea2a504bcaad81155281e3f5308fbb
ghsssss <947409601@qq.com>
2025-01-13 10:53:15 +0800

    更新请求地址
    然后加入ts参数，用于服务器的时间校验

--- mt_vote.py
+++ mt_vote.py
@@ -2,15 +2,18 @@ import requests
 import re
 import os
 import json
+import time
 from notify import send
 
 # 投票接口
-URL = 'https://api2.m-team.cc/api/fun/first'
+URL = 'https://api.m-team.cc/api/fun/first'
 # 从环境变量中获取cookie
 cookie = os.environ.get('MT_COOKIE')
+current_ts =  str(int(time.time()))
+
 # 构造请求头
 HEADERS = {
-    'authority': 'api2.m-team.cc',
+    'authority': 'api.m-team.cc',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
     'cache-control': 'max-age=0',
@@ -25,7 +28,9 @@ HEADERS = {
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.46',
     'Authorization': cookie,
-    'Host':'api2.m-team.cc'
+    'Host':'api.m-team.cc',
+    "referer":"https://kp.m-team.cc/",
+    "ts":current_ts
 }
 
 # 获取投票id
@@ -49,7 +54,7 @@ def main():
     # 获取投票id
     voId = get_voId()
     # 构造投票请求
-    url = "https://api2.m-team.cc/api/fun/vote"
+    url = "https://api.m-team.cc/api/fun/vote"
     # 发送投票请求
     response = requests.post(url,{'funid':voId,'opinion':'true'},headers=HEADERS)
     # 判断请求是否成功
